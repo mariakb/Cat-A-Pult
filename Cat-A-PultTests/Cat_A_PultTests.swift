fece8fa221094748efca044e63de9464061f70b9
Maria <maria.karg95@gmail.com>
2019-04-22 19:39:22 -0400

    Initial Commit

---
+++ Cat-A-PultTests/Cat_A_PultTests.swift
@@ -0,0 +1,34 @@
+//
+//  Cat_A_PultTests.swift
+//  Cat-A-PultTests
+//
+//  Created by Karg, Maria on 4/22/19.
+//  Copyright © 2019 Karg, Maria. All rights reserved.
+//
+
+import XCTest
+@testable import Cat_A_Pult
+
+class Cat_A_PultTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
